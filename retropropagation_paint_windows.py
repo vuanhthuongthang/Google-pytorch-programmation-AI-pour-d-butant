04f669c6526cb6f6080f657c6fa83397319261d2
vuanhthuongthang <vuthang@hotmail.fr>
2026-08-15 06:26:58 +0200

    Add retropropagation image reconstruction script
    Implement image reconstruction using retropropagation with PyTorch.

---
+++ retropropagation_paint_windows.py
@@ -0,0 +1,34 @@
+import torch
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+
+torch.manual_seed(42)
+nom_fichier_entree = "mon_dessin.png"
+nom_fichier_sortie = "mon_dessin_reconstruit.png"
+
+image_pil = Image.open(nom_fichier_entree).convert('L').resize((28, 28))
+image_tensor = transforms.ToTensor()(image_pil)
+VRAI_A = 1.0 - image_tensor.squeeze()
+
+X = torch.rand((28, 800))
+Y = torch.matmul(VRAI_A, X)
+
+A_estime = torch.rand((28, 28), requires_grad=True)
+taux_apprentissage = 0.005
+
+for etape in range(1200):
+    loss = 0.5 * torch.sum((torch.matmul(A_estime, X) - Y) ** 2)
+    loss.backward()
+    with torch.no_grad():
+        A_estime -= taux_apprentissage * A_estime.grad
+    A_estime.grad.zero_()
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.imshow(VRAI_A.detach().numpy(), cmap='gray')
+ax1.set_title("Original (Paint)")
+ax2.imshow(A_estime.detach().numpy(), cmap='gray')
+ax2.set_title("Reconstruit")
+plt.savefig(nom_fichier_sortie, dpi=150)
+plt.show() # Ouvre la fenêtre sous Windows 11
